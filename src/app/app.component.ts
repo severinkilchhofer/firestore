257f0928c6b3600792a797a9753288c1048fe527
Severin Kilchhofer <>
2018-03-22 20:09:25 +0100

    first push

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
+
+  title: string;
+  content: string;
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.valueChanges();
+  }
+
+  addPost() {
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+  }
+}
